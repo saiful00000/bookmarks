13e3a31422d30b8a5d5fe532f96ed1b1136cea16
shaiful islam <saiful161003@gmail.com>
2022-05-21 13:21:08 +0600

    database migration with user table modification

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { AuthDto } from "./dto";
 import * as argon from "argon2"
 import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 
 @Injectable({})
@@ -9,30 +10,32 @@ export class AuthService {
     constructor(private prisma: PrismaService) { }
 
     async signup(dto: AuthDto) {
-        // generate the password
-        const hash = await argon.hash(dto.password);
+        try {
+            // generate the password
+            const hash = await argon.hash(dto.password);
 
-        // save new user to database
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash: hash,
-                first_name: dto.first_name,
-                last_name: dto.last_name,
-            },
-            select: {
-                id: true,
-                email: true,
-                first_name: true,
-                last_name: true,
-                created_at: true,
-                updated_at: true
+            // save new user to database
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash: hash,
+                    first_name: dto.first_name,
+                    last_name: dto.last_name,
+                },
+            });
+
+            delete user.hash;
+
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code = 'p2002') {
+                    throw new ForbiddenException('Credential already taken.');
+                }
             }
-        });
-
-        return user;
+            throw error;
+        }
     }
-
     login(email: string) {
         return { 'login': 'login success.' };
     }
